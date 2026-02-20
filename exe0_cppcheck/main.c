76b3d160c4257418254627524fb50faf845e32c9
denis <denisalonso1999@gmail.com>
2026-02-20 09:18:52 -0300

    teste

--- exe0_cppcheck/main.c
+++ exe0_cppcheck/main.c
@@ -5,11 +5,12 @@
 
 int multiplyNumbers(int x, int y) {
     int product = x * y;
+    return product;
 }
 
 int main(void) {
     int a, b; //uninitvar, legacyUninitvar
-    int result = multiplyNumbers(a, b);
+    int result = multiplyNumbers(a,b);
 
     int arr[NUM_ELEMENTS];
     for(int i = 0; i <= NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
