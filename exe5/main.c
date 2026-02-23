ab525fa6afaf55cfb1be996f4bb4582233a9bd85
denis <denisalonso1999@gmail.com>
2026-02-23 14:21:55 -0300

    ex 5

--- exe5/main.c
+++ exe5/main.c
@@ -2,30 +2,40 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
-const int BTN_PIN = 26;
-const int BTN_PIN_2 = 7;
+const int BTN1 = 26;
+const int BTN2 = 7;
 
 int main() {
     stdio_init_all();
 
-    gpio_init(BTN_PIN);
-    gpio_set_dir(BTN_PIN, GPIO_IN);
-    gpio_pull_up(BTN_PIN);
+    gpio_init(BTN1);
+    gpio_set_dir(BTN1, GPIO_IN);
+    gpio_pull_up(BTN1);
 
-    gpio_init(BTN_PIN_2);
-    gpio_set_dir(BTN_PIN_2, GPIO_IN);
-    gpio_pull_up(BTN_PIN_2);
+    gpio_init(BTN2);
+    gpio_set_dir(BTN2, GPIO_IN);
+    gpio_pull_up(BTN2);
+
     int cnt_1 = 0;
     int cnt_2 = 0;
+    bool last_state_btn1 = true;
+    bool last_state_btn2 = true;
 
     while (true) {
-
-        if (!gpio_get(BTN_PIN)) {
-            printf("Botao 1: %d\n", cnt_1++);
+        bool current_state_1 = gpio_get(BTN1);
+        if (last_state_btn1 == true && current_state_1 == false) {
+            cnt_1++;
+            printf("Botao 1: %d\n", cnt_1);
         }
+        last_state_btn1 = current_state_1;
+        sleep_ms(10);
 
-        if (!gpio_get(BTN_PIN_2)) {
-            printf("Botao 2: %d\n", cnt_2++);
+        bool current_state_2 = gpio_get(BTN2);
+        if (last_state_btn2 == true && current_state_2 == false) {
+            cnt_2++;
+            printf("Botao 2: %d\n", cnt_2);
         }
+        last_state_btn2 = current_state_2;
+        sleep_ms(10);
     }
 }
