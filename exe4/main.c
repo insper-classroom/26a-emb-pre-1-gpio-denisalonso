f81b5653c676cc50975197dc0ee8dc1a7c7c16da
denis <denisalonso1999@gmail.com>
2026-02-23 12:56:36 -0300

    ex 4 funcionando...

--- exe4/main.c
+++ exe4/main.c
@@ -2,10 +2,48 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int LED0 = 5;
+const int LED1 = 8;
+const int LED2 = 11;
+const int LED3 = 15;
+const int BTN = 28;
+
+
 int main() {
+  
   stdio_init_all();
+  gpio_init(BTN);
+  gpio_set_dir(BTN, GPIO_IN);
+  gpio_pull_up(BTN);
+
+  gpio_init(LED0);
+  gpio_set_dir(LED0, GPIO_OUT);
+  gpio_init(LED1);
+  gpio_set_dir(LED1, GPIO_OUT);
+  gpio_init(LED2);
+  gpio_set_dir(LED2, GPIO_OUT);
+  gpio_init(LED3);
+  gpio_set_dir(LED3, GPIO_OUT);
+  
 
   while (true) {
-    // Use delay de 300 ms entre os estados!
+    if (!gpio_get(BTN)) {
+      gpio_put(LED0, 1);
+      sleep_ms(300);
+      gpio_put(LED1, 1);
+      gpio_put(LED0, 0);
+      sleep_ms(300);
+      gpio_put(LED2, 1);
+      gpio_put(LED1, 0);
+      sleep_ms(300);
+      gpio_put(LED3, 1);
+      gpio_put(LED2, 0);
+      sleep_ms(300);
+      gpio_put(LED3, 0);
+
+      // espera o botão ser solto (evita repetir enquanto estiver segurando)
+      while (!gpio_get(BTN)) {
+      }
+    }
   }
 }
